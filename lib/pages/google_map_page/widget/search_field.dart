fdfecef86e8b2ff4db5f9fc4dbff3306d2b1ba46
shokiogawa <shouki2578@gmail.com>
2024-05-18 15:37:35 +0900

    Ref: 単方向データフローチックに修正

--- lib/pages/google_map_page/widget/search_field.dart
+++ lib/pages/google_map_page/widget/search_field.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map_app/features/charger_spot/provider/fetch_charger_spot_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../../common/provider/google_map_controller_provider.dart';
+
 class SearchField extends HookConsumerWidget {
   const SearchField({super.key});
 
@@ -15,9 +17,20 @@ class SearchField extends HookConsumerWidget {
         onTap: chargerSpotAsyncValue.isLoading
             ? null
             : () async {
+                // 現在見えている範囲を取得
+                final visibleRegionArea =
+                    await (await ref.watch(googleMapControllerProvider).future)
+                        .getVisibleRegion();
+
+                // 再検索
                 ref
                     .read(fetchChargerSpotNotifierProvider.notifier)
-                    .searchChargerSpot();
+                    .searchChargerSpot(
+                        swLat: visibleRegionArea.southwest.latitude.toString(),
+                        swLng: visibleRegionArea.southwest.longitude.toString(),
+                        neLat: visibleRegionArea.northeast.latitude.toString(),
+                        neLng:
+                            visibleRegionArea.northeast.longitude.toString());
               },
         child: Card(
           shape:
@@ -34,8 +47,8 @@ class SearchField extends HookConsumerWidget {
                       chargerSpotAsyncValue.isLoading
                           ? "検索中..."
                           : "このエリアでスポットを検索",
-                      style:
-                          const TextStyle(fontSize: 15, color: Color(0xff56C600)),
+                      style: const TextStyle(
+                          fontSize: 15, color: Color(0xff56C600)),
                     ),
                   ),
                   const Flexible(
